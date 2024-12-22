9812754c0723b164f9dab8c3a0475bb6de44ba79
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-12-22 00:35:03 -0500

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Monkey Config
+
+```kotlin
+val configManager = tomlConfigManager("myFileConfig", somePath.resolve("test.toml")) {
+    booleanSetting("monkeyStyle", true) {
+        comment = "This is true by default!"
+    }
+    section("preferences") {
+        comment = "These settings are for preferences."
+        numberSetting<Int>("breakDaysLimit", 0) {
+            comment = "How many days I can take a break before getting back in the grind. Default is 0. Minimum is 0 and maximum is 10."
+            minValue = 0
+            maxValue = 10
+            implementation = ::TestNumberSetting
+        }
+        section("whitelist") {
+            stringSetting("prompt", "You need to be whitelisted!") {
+                comment = "The prompt shown to users who are not whitelisted."
+            }
+            booleanSetting("enabled", true) {
+                comment = "Default is true"
+            }
+            listSetting<String, StringType>("list", listOf("ArkoSammy12")) {
+                implementation = ::StringListSetting
+                serializer = { list -> ListType<StringType>(list.map { e -> StringType(e) }.toList()) }
+                deserializer = { serializedList -> serializedList.value.map { e -> e.value.toString() }.toList()  }
+            }
+        }
+    }
+}
+configManager.saveToFile()
+```
--- src/test/kotlin/io/arkosammy12/monkeyconfig/Main.kt
+++ src/test/kotlin/io/arkosammy12/monkeyconfig/Main.kt
@@ -1,7 +1,10 @@
 package io.arkosammy12.monkeyconfig
 
 import io.arkosammy12.monkeyconfig.builders.tomlConfigManager
+import io.arkosammy12.monkeyconfig.settings.StringListSetting
 import io.arkosammy12.monkeyconfig.settings.TestNumberSetting
+import io.arkosammy12.monkeyconfig.types.ListType
+import io.arkosammy12.monkeyconfig.types.StringType
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -18,23 +21,30 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val configManager = tomlConfigManager("testConfig", TEST_PATH.resolve("test.toml")) {
-            booleanSetting("testTopLevelBooleanSetting", true) {
-                comment = "Test comment"
+        val configManager = tomlConfigManager("myFileConfig", TEST_PATH.resolve("test.toml")) {
+            booleanSetting("monkeyStyle", true) {
+                comment = "This is true by default!"
             }
 
-            section("testSection") {
-                comment = "Test comment here"
-                numberSetting<Int>("testNumberSetting", 0) {
-                    comment = "Test comment"
+            section("preferences") {
+                comment = "These settings are for preferences."
+                numberSetting<Int>("breakDaysLimit", 0) {
+                    comment = "How many days I can take a break before getting back in the grind. Default is 0. Minimum is 0 and maximum is 10."
                     minValue = 0
                     maxValue = 10
                     implementation = ::TestNumberSetting
                 }
-                section("testSubsection") {
-                    comment = "Test comment"
-                    stringSetting("testStringSetting", "hello_dicedpixels") {
-                        comment = "Test comment"
+                section("whitelist") {
+                    stringSetting("prompt", "You need to be whitelisted!") {
+                        comment = "The prompt shown to users who are not whitelisted."
+                    }
+                    booleanSetting("enabled", true) {
+                        comment = "Default is true"
+                    }
+                    listSetting<String, StringType>("list", listOf("ArkoSammy12")) {
+                        implementation = ::StringListSetting
+                        serializer = { list -> ListType<StringType>(list.map { e -> StringType(e) }.toList()) }
+                        deserializer = { serializedList -> serializedList.value.map { e -> e.value.toString() }.toList()  }
                     }
                 }
             }
