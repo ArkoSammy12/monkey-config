d31147f601c250b7f2d6f987fe55cd2fc9904747
ArkoSammy12 <jamesvillarreal12@gmail.com>
2025-03-22 17:52:01 -0500

    Change the type of the value property in NumberSetting.kt to NumberSettingValue

--- src/main/kotlin/io/github/arkosammy12/monkeyconfig/settings/NumberSetting.kt
+++ src/main/kotlin/io/github/arkosammy12/monkeyconfig/settings/NumberSetting.kt
@@ -3,13 +3,12 @@ package io.github.arkosammy12.monkeyconfig.settings
 import io.github.arkosammy12.monkeyconfig.builders.NumberSettingBuilder
 import io.github.arkosammy12.monkeyconfig.types.NumberType
 import io.github.arkosammy12.monkeyconfig.values.NumberSettingValue
-import io.github.arkosammy12.monkeyconfig.values.SettingValue
 
 open class NumberSetting<T : Number>(
     settingBuilder: NumberSettingBuilder<T>,
 ) : AbstractSetting<T, NumberType<T>, NumberSetting<T>>(settingBuilder) {
 
-    override val value: SettingValue<T, NumberType<T>> =
+    override val value: NumberSettingValue<T> =
         NumberSettingValue(settingBuilder.defaultValue, minValue = settingBuilder.minValue, maxValue = settingBuilder.maxValue, serializer = settingBuilder.serializer, deserializer = settingBuilder.deserializer)
 
     override fun onInitialized() {
